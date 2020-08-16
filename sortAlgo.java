d86ea4fcd91468b1153a93e96512dbf5b064d5fd
mohamedm1 <63683274+mohamedm1@users.noreply.github.com>
2020-08-16 17:59:26 -0400

    Create sortAlgo.java

---
+++ sortAlgo.java
@@ -0,0 +1,46 @@
+
+public class sortAlgo {
+
+	public static void main(String[] args) {
+		
+		int [] array = {1,29,14,15,94};
+		   int num;
+		   for (int i = 1; i < array.length; i++) {
+		    for (int j = i; j > 0; j--) {
+		     if (array[j] < array [j - 1]) {
+		      num = array[j];
+		      array[j] = array[j - 1];
+		      array[j - 1] = num;
+		     }
+		    }
+		   }
+		   for (int i = 0; i < array.length; i++) {
+		     System.out.println(array[i]);
+		   }
+		  }
+		
+
+public static int[] doSelectionSort(int[] arr){
+
+for (int i = 0; i < arr.length - 1; i++)
+{
+ int index = i;
+ for (int j = i + 1; j < arr.length; j++)
+     if (arr[j] < arr[index]) 
+         index = j;
+
+ int lowernum= arr[index];  
+ arr[index] = arr[i];
+ arr[i] = lowernum;
+}
+return arr;
+}
+
+int[] arr1 = {1, 29, 14, 15, 96};
+int[] arr2 = doSelectionSort(arr1); {
+for(int i:arr2){
+	System.out.print(i);
+	System.out.print(", ");
+}
+}
+}
